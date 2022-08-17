e90d887f4dd7513ecd0b4fbe6b5efdfdb5f57f61
Chookity96 <jegathees35@gmail.com>
2022-08-17 17:38:56 +0800

    updated

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,7 +5,12 @@ function Item({ item, itemToUpdate, deleteItem }) {
         <div className="card">
             <div className="content">
                 <div className="contentBx">
-                    <h3>{item?.name}<br /><span>${(+item?.price).toFixed(2)}</span></h3>
+                    <h3>{item?.name}
+                    <br />
+                    <span>${(+item?.price).toFixed(2)}</span>
+                    <br />
+                    <span>ID: {item?.id.slice(0, 8)}</span> 
+                    </h3>
                 </div>
             </div>
             <ul className="sci">
